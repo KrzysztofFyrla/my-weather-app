2fa9a6e20ccc378ae7aa8e30f4d7eef082f584a5
Krzysztof F <krzysztoffyrla@gmail.com>
2020-07-14 22:29:48 +0200

    first commit

---
+++ src/main/java/pl/krzysztoffyrla/myweatherapp/controller/WeatcherService.java
@@ -0,0 +1,76 @@
+package pl.krzysztoffyrla.myweatherapp.controller;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+/**
+ * @author Krzysztof
+ * @project my-weather-app
+ */
+public class WeatcherService {
+
+    private OkHttpClient client;
+    private Response response;
+    private String CityName;
+    String unit;
+    private String API = "";
+
+    public JSONObject getWeatcher() {
+        client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://api.openweathermap.org/data/2.5/weather?q=" + getCityName() + "&units=" + getUnit()
+                        + "")
+                .build();
+
+        try {
+            response = client.newCall(request).execute();
+            return new JSONObject(response.body().string());
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public JSONArray returnWeatherArray() throws JSONException {
+        JSONArray weatherArray = getWeatcher().getJSONArray("weather");
+        return weatherArray;
+    }
+
+    public JSONObject returnMain() throws JSONException {
+        JSONObject main = getWeatcher().getJSONObject("main");
+        return main;
+    }
+
+    public JSONObject returnWind() throws JSONException {
+        JSONObject wind = getWeatcher().getJSONObject("wind");
+        return wind;
+    }
+
+    public JSONObject returnSys() throws JSONException {
+        JSONObject sys = getWeatcher().getJSONObject("sys");
+        return sys;
+    }
+
+    public String getCityName() {
+        return CityName;
+    }
+
+    public void setCityName(String cityName) {
+        CityName = cityName;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+}
---
+++ src/main/java/pl/krzysztoffyrla/myweatherapp/gui/MainGui.java
@@ -0,0 +1,60 @@
+package pl.krzysztoffyrla.myweatherapp.gui;
+
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.router.Route;
+
+import java.util.ArrayList;
+
+/**
+ * @author Krzysztof
+ * @project my-weather-app
+ */
+@Route("hello")
+@StyleSheet("/css/style.css")
+public class MainGui extends VerticalLayout {
+
+    public MainGui() {
+        setHeader();
+        setLogo();
+        setForm();
+    }
+
+    public void setHeader() {
+        Label title = new Label("My Weather Application");
+
+        HorizontalLayout headerLabel = new HorizontalLayout(title);
+        headerLabel.setJustifyContentMode(JustifyContentMode.CENTER);
+        headerLabel.setWidth("100%");
+        add(headerLabel);
+    }
+
+    public void setLogo() {
+        Image img = new Image("/css/logo.png", "weatcher");
+        img.setWidth("240px");
+        img.setHeight("240px");
+
+        HorizontalLayout headerImg = new HorizontalLayout(img);
+        headerImg.setJustifyContentMode(JustifyContentMode.CENTER);
+        headerImg.setWidth("100%");
+        add(headerImg);
+    }
+
+    public void setForm() {
+        Select<String> selectTemperature = new Select<>();
+        ArrayList<String> items = new ArrayList<>();
+        items.add("C");
+        items.add("F");
+        selectTemperature.setItems(items);
+        selectTemperature.setValue(items.get(0));
+
+        HorizontalLayout headerSelector = new HorizontalLayout(selectTemperature);
+        headerSelector.setJustifyContentMode(JustifyContentMode.CENTER);
+        headerSelector.setWidth("100%");
+        add(headerSelector);
+    }
+}
